9dd32a1ac9cf900a8e09a5ee07052ffb4969002a
khainsa <146795922+khainsa@users.noreply.github.com>
2025-05-20 00:49:48 +0700

    masukkan data ke patient

--- database/migrations/2025_05_19_034400_create_patients_tabel.php
+++ database/migrations/2025_05_19_034400_create_patients_tabel.php
@@ -8,20 +8,20 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('patients', function (Blueprint $table) {
-        $table->id();
-        $table->unsignedInteger('user_id');
-        $table->string('nama_lengkap', 100);
-        $table->string('nik', 20);
-        $table->date('tanggal_lahir');
-        $table->enum('jenis_kelamin', ['L', 'P']);
-        $table->string('golongan_darah', 3)->nullable();
-        $table->string('email', 100)->nullable();
-        $table->string('no_hp', 20)->nullable();
-        $table->text('alamat')->nullable();
-        $table->timestamp('created_at')->useCurrent();
-        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-    });
+    Schema::create('patients', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+    $table->string('nama_lengkap', 100);
+    $table->string('nik', 20);
+    $table->date('tanggal_lahir');
+    $table->enum('jenis_kelamin', ['L', 'P']);
+    $table->string('golongan_darah', 3)->nullable();
+    $table->string('email', 100)->nullable();
+    $table->string('no_hp', 20)->nullable();
+    $table->text('alamat')->nullable();
+    $table->timestamps();
+});
+
     }
 
     public function down(): void
