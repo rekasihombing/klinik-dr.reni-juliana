670e148d7a545966776dd87503e4c65af54ef3a6
unknown <rekasihombing19@gmail.com>
2025-05-20 14:43:59 +0700

    takut nabrak

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('kontak', function () {
+    return Inertia::render('Kontak');
+})->name('kontak');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
